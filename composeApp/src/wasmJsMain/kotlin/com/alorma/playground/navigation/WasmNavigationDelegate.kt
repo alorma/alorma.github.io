a1e57e6e23312f487659c258f2cc55ec1d570ee8
Bernat Borrás <bernatbor15@gmail.com>
2025-12-28 14:23:17 +0100

    Wasm navigation

---
+++ composeApp/src/wasmJsMain/kotlin/com/alorma/playground/navigation/WasmNavigationDelegate.kt
@@ -0,0 +1,9 @@
+package com.alorma.playground.navigation
+
+import kotlinx.browser.window
+
+class WasmNavigationDelegate : NavigationDelegate {
+    override fun openUrl(url: String) {
+        window.open(url, "_blank")
+    }
+}
--- composeApp/src/wasmJsMain/kotlin/com/alorma/playground/platform/di/PlatformModule.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/com/alorma/playground/platform/di/PlatformModule.wasmJs.kt
@@ -1,8 +1,12 @@
 package com.alorma.playground.platform.di
 
+import com.alorma.playground.navigation.NavigationDelegate
+import com.alorma.playground.navigation.WasmNavigationDelegate
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 actual val platformModule: Module = module {
-
+  singleOf(::WasmNavigationDelegate) { bind<NavigationDelegate>() }
 }
